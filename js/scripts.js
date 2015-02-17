c4923a3c0996c7a14bec001bcf919e85da9416e1
Mike Harris & Arsi <student@epicodus.com>
2015-02-17 10:22:18 -0800

    Add full address function

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,18 @@ $(document).ready(function() {
 
     var inputFirstName = $("input#new-first-name").val();
     var inputLastName = $("input#new-last-name").val();
-    var inputAddress = $("input#new-address").val();
+
+    var inputStreet = $("input#new-street").val();
+    var inputCity = $("input#new-city").val();
+    var inputState = $("input#new-state").val();
+    var inputZip = $("input#new-zip").val();
+    var newAddress = { street: inputStreet, city: inputCity, state: inputState, zip: inputZip,
+                       fullAddress: function() {
+                         return this.street + ", " + this.city + ", " + this.state + ", " + this.zip;
+                       }
+                      };
     var newContact = {firstName: inputFirstName,
-                      lastName: inputLastName, address: inputAddress};
+                      lastName: inputLastName, address: newAddress.fullAddress()};
 
     $('ul#contacts').append("<li><span class ='contact'>" +
                             newContact.firstName + " " +
@@ -14,7 +23,10 @@ $(document).ready(function() {
 
     $('input#new-first-name').val("");
     $('input#new-last-name').val("");
-    $('input#new-address').val("");
+    $("input#new-street").val("");
+    $("input#new-city").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
 
     $(".contact").last().click(function(){
       $('#show-contact').show();
@@ -23,6 +35,7 @@ $(document).ready(function() {
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
       $(".address").text(newContact.address);
+
     });
   });
 });
